8ff4494da78906702c34c628ba8fe3c8c636cf0f
EliNygard <eli.nygard@gmail.com>
2024-04-29 15:15:36 +0200

    adding js folders, creating constant api

---
+++ javascript/api/constantAPI.mjs
@@ -0,0 +1,5 @@
+const API_BASE_URL = 'https://v2.api.noroff.dev';
+
+export const API_POSTS_URL = `${API_BASE_URL}/blog/posts/<name>`;
+// is <name> supposed to be here? is <name> a placeholder for a variable?
+
