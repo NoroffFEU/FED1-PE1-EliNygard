60e71c6b66a9f7aedb1ad043fcceaad9dd247ff7
EliNygard <eli.nygard@gmail.com>
2024-04-30 12:34:01 +0200

    adding register user js

--- javascript/api/register.mjs
+++ javascript/api/register.mjs
@@ -1,10 +1,16 @@
 import { API_AUTH, API_BASE, API_REGISTER } from "./constantAPI.mjs";
 
-
-
-
 async function registerUser (url, userData) {
     try {
+
+        // const existingUserResponse = await fetch(url + `?email=${userData.email}`);
+        // const existingUserJson = await existingUserResponse.json();
+
+        // if (existingUserJson.exists) {
+        //     alert('User with the same email already exists');
+        //     return;
+        // }
+
         const postData = {
             method: 'POST',
             headers: {
@@ -14,9 +20,8 @@ async function registerUser (url, userData) {
             };
         const response = await fetch(url, postData);
         const json = await response.json();
-            console.log(json);
         return json;
-        } 
+    } 
         
         catch (error) {
             console.error('Error:', error);
@@ -24,10 +29,38 @@ async function registerUser (url, userData) {
 }
 
 
-const userData = {
-    name: 'eli_nyg',
-    email: 'elinyg@stud.noroff.no',
-    password: 'Test3Test5'
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const registerForm = document.getElementById('js-registration-form');
+    
+    registerForm.addEventListener('submit', async function (event) {
+        event.preventDefault(); //prevents the default form submission behavior
+        
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirm-password').value;
+        
+        if (password !== confirmPassword) {
+            alert("Passwords do not match! Try again");
+            return;
+        };
+        
+        const userData = {
+            name: name,
+            email: email,
+            password: password
+        };
+        
+        await registerUser(API_BASE + API_AUTH + API_REGISTER, userData);
+        
+    });
+});
 
-registerUser(API_BASE + API_AUTH + API_REGISTER, userData);
+
+// const userData = {
+//     name: 'lelinyga',
+//     email: 'lelinyga@stud.noroff.no',
+//     password: 'Test1234'
+// }
+
+// registerUser(API_BASE + API_AUTH + API_REGISTER, userData);
