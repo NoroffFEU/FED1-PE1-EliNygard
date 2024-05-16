6b20666ba20fdc3b4b7150190f6201999953d824
EliNygard <eli.nygard@gmail.com>
2024-05-16 08:12:37 +0200

    activated prettier, added ;

--- javascript/generateHtml/postPageHtml.mjs
+++ javascript/generateHtml/postPageHtml.mjs
@@ -2,65 +2,68 @@ import { copyUrlMessage } from "../messages/copyUrlMessage.mjs";
 import { formatDate, removeUnderscore } from "../ui/formatting.mjs";
 
 export function generatePostPageHtml(post) {
-    // const main = document.querySelector("main")
+  // const main = document.querySelector("main")
 
-    const postWrapper = document.createElement("section")
-    postWrapper.classList.add("post-wrapper")
+  const postWrapper = document.createElement("section");
+  postWrapper.classList.add("post-wrapper");
 
-    const img = document.createElement("img")
-    img.src = post.media.url;
-    img.alt = post.media.alt;
+  const img = document.createElement("img");
+  img.src = post.media.url;
+  img.alt = post.media.alt;
 
-    const postContainer = document.createElement("div")
-    postContainer.classList.add("post-container")
+  const postContainer = document.createElement("div");
+  postContainer.classList.add("post-container");
 
-    const title = document.createElement("h1")
-    title.classList.add("font-secondary", "heading-secondary")
-    title.textContent = post.title;
+  const title = document.createElement("h1");
+  title.classList.add("font-secondary", "heading-secondary");
+  title.textContent = post.title;
 
-    const postDetails = document.createElement("div")
-    postDetails.classList.add("post-details", "font-secondary")
+  const postDetails = document.createElement("div");
+  postDetails.classList.add("post-details", "font-secondary");
 
-    const author = document.createElement("p")
-    author.setAttribute("id", "post-author")
-    author.textContent = removeUnderscore(post.author.name);
+  const author = document.createElement("p");
+  author.setAttribute("id", "post-author");
+  author.textContent = removeUnderscore(post.author.name);
 
-    const date = document.createElement("p")
-    date.setAttribute("id", "post-date")
-    date.classList.add("font-secondary")
-    if (post.updated === post.created) {
-        const formattedDate = post.created;
-        date.textContent = formatDate(formattedDate);
-    } else {
-        const formattedDate = post.updated;
-        date.textContent = formatDate(formattedDate);
-    }
+  const date = document.createElement("p");
+  date.setAttribute("id", "post-date");
+  date.classList.add("font-secondary");
+  if (post.updated === post.created) {
+    const formattedDate = post.created;
+    date.textContent = formatDate(formattedDate);
+  } else {
+    const formattedDate = post.updated;
+    date.textContent = formatDate(formattedDate);
+  }
 
-    const iconCopyContainer = document.createElement("div")
-    iconCopyContainer.classList.add("icon-copy-container")
+  const iconCopyContainer = document.createElement("div");
+  iconCopyContainer.classList.add("icon-copy-container");
 
-    const iconCopy = document.createElement("span")
-    iconCopy.classList.add("share-icon", "fa-solid", "fa-link")
-    iconCopy.setAttribute("title", "Copy link")
-    iconCopy.addEventListener('click', () => {
-        const postUrl = window.location.href; //current page url
-        const copyUrl = postUrl;
-        navigator.clipboard.writeText(copyUrl).then(() => {
-            iconCopyContainer.appendChild(copyUrlMessage())
-        }).catch((error) => {
-            console.error("failed to copy url"), error;
-        });
-    });
+  const iconCopy = document.createElement("span");
+  iconCopy.classList.add("share-icon", "fa-solid", "fa-link");
+  iconCopy.setAttribute("title", "Copy link");
+  iconCopy.addEventListener("click", () => {
+    const postUrl = window.location.href; //current page url
+    const copyUrl = postUrl;
+    navigator.clipboard
+      .writeText(copyUrl)
+      .then(() => {
+        iconCopyContainer.appendChild(copyUrlMessage());
+      })
+      .catch((error) => {
+        console.error("failed to copy url"), error;
+      });
+  });
 
-    const bodyText = document.createElement("p")
-    bodyText.classList.add("post-content", "font-primary", "body-text")
-    bodyText.textContent = post.body;
+  const bodyText = document.createElement("p");
+  bodyText.classList.add("post-content", "font-primary", "body-text");
+  bodyText.textContent = post.body;
 
-    // main.appendChild(postWrapper)
-    postDetails.append(author, date, iconCopyContainer)
-    iconCopyContainer.appendChild(iconCopy)
-    postContainer.append(title, postDetails, bodyText)
-    postWrapper.append(img, postContainer)
+  // main.appendChild(postWrapper)
+  postDetails.append(author, date, iconCopyContainer);
+  iconCopyContainer.appendChild(iconCopy);
+  postContainer.append(title, postDetails, bodyText);
+  postWrapper.append(img, postContainer);
 
-    return postWrapper;
+  return postWrapper;
 }
