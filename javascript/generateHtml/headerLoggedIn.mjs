29c5b36dc9c4fdc58d2a6190a4a8d9c8968c0c6a
EliNygard <eli.nygard@gmail.com>
2024-05-21 14:46:25 +0200

    generating header html

--- javascript/generateHtml/headerLoggedIn.mjs
+++ javascript/generateHtml/headerLoggedIn.mjs
@@ -1,3 +1,23 @@
 export function generateHeaderLoggedInHtml() {
-const header = document.getElementById
+  const header = document.getElementById("header-logged-in");
+  header.classList.add("bg-color-theme-light");
+
+  const nav = document.createElement("nav");
+  nav.classList.add("h-f-container", "header-container", "font-primary");
+
+  const leftContainer = document.createElement("div");
+  leftContainer.classList.add("content-left");
+
+  const linkHome = document.createElement("a");
+  linkHome.href = "../index.html";
+  linkHome.setAttribute("title", "HotView Labs Blog");
+  linkHome.textContent = "HotView Labs";
+
+  const rightContainer = document.createElement("div");
+  rightContainer.classList.add("content-right");
+
+  const userName = document.createElement("p");
+
+  const logOutBtn = document.createElement("button");
+  logOutBtn.classList.add("button", "button-small");
 }
