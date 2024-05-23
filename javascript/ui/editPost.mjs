21a6f6c8456a8fbddd135e68d3de6418ef8b6010
EliNygard <eli.nygard@gmail.com>
2024-05-23 12:24:59 +0200

    styling on img in carousel

--- javascript/ui/editPost.mjs
+++ javascript/ui/editPost.mjs
@@ -89,7 +89,6 @@ form.addEventListener("submit", function (event) {
     .catch((error) => {
       alert(error.message); //display error to user
       console.error(error); // Log errors in console
-      // console.error(json.errors);
     });
 });
 
