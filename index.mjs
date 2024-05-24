bb3c329909711281b2b77bf86cc0aa42cfc71dbe
EliNygard <eli.nygard@gmail.com>
2024-05-24 10:36:58 +0200

    adding sort descending and ascending

--- index.mjs
+++ index.mjs
@@ -42,18 +42,43 @@ await checkAndRenderPosts();
 
 // sort by date function:
 function sortPostsByDate(posts) {
-  const button = document.querySelector(".sort-by-date-descending");
-  button.addEventListener("click", () => {
-      const sortedPosts = posts.sort((b, a) => {
-        const dateA = new Date(a.updated || a.created);
-        const dateB = new Date(b.updated || b.created);
-        return dateA - dateB;
-      });
-
-      const paginatedPosts = paginate(sortedPosts, 10);
-      renderPosts(paginatedPosts[0]);
-      renderPaginationControls(paginatedPosts, sortedPosts);
+  const buttonDesc = document.querySelector(".sort-by-date-descending");
+  const buttonAsc = document.querySelector(".sort-by-date-ascending");
+  // button.addEventListener("click", () => {
+  const sortDescending = () => {
+    const sortedPosts = posts.sort((a, b) => {
+      const dateA = new Date(a.updated || a.created);
+      const dateB = new Date(b.updated || b.created);
+      return dateB - dateA;
     });
+    const paginatedPosts = paginate(sortedPosts, 4);
+    renderPosts(paginatedPosts[0]);
+    renderPaginationControls(paginatedPosts, sortedPosts);
+  };
+
+  const sortAscending = () => {
+    const sortedPosts = posts.sort((a, b) => {
+      const dateA = new Date(a.updated || a.created);
+      const dateB = new Date(b.updated || b.created);
+      return dateA - dateB;
+    });
+    const paginatedPosts = paginate(sortedPosts, 4);
+    renderPosts(paginatedPosts[0]);
+    renderPaginationControls(paginatedPosts, sortedPosts);
+  };
+
+  buttonAsc.addEventListener("click", sortAscending);
+  buttonDesc.addEventListener("click", sortDescending);
+  //   const sortedPosts = posts.sort((a, b) => {
+  //     const dateA = new Date(a.updated || a.created);
+  //     const dateB = new Date(b.updated || b.created);
+  //     return dateA - dateB;
+  //   });
+
+  //   const paginatedPosts = paginate(sortedPosts, 10);
+  //   renderPosts(paginatedPosts[0]);
+  //   renderPaginationControls(paginatedPosts, sortedPosts);
+  // });
 }
 
 async function renderPosts(posts) {
