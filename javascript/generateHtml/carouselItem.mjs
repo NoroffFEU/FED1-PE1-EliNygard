da9c5fcfd62b3f5fe2b5cd7777f86f8dcd1d2914
EliNygard <eli.nygard@gmail.com>
2024-05-24 13:45:29 +0200

    adding link href to carousel item

--- javascript/generateHtml/carouselItem.mjs
+++ javascript/generateHtml/carouselItem.mjs
@@ -2,8 +2,25 @@ export function generateCarouselItem(post) {
   const slideContainer = document.createElement("li");
   slideContainer.classList.add("slide-container", "fade-to-next");
 
-  const newArticleContainer = document.createElement("div");
+  const newArticleContainer = document.createElement("a");
   newArticleContainer.classList.add("new-article");
+  newArticleContainer.addEventListener("click", (event) => {
+    event.preventDefault()
+    event.preventDefault();
+    const postId = post.id;
+    const newUrl = `./post/index.html?postId=${postId}`;
+    window.location.assign(newUrl);
+  })
+
+  // const LinkWrapper = document.createElement("a")
+  // LinkWrapper.addEventListener("click", (event) => {
+  //   event.preventDefault()
+  //   event.preventDefault();
+  //   const postId = post.id;
+  //   const newUrl = `./post/index.html?postId=${postId}`;
+  //   window.location.assign(newUrl);
+  // })
+
 
   const img = document.createElement("img");
   img.src = post.media.url;
@@ -18,17 +35,17 @@ export function generateCarouselItem(post) {
   const body = document.createElement("p");
   body.classList.add("font-primary", "body-text", "text-overflow");
   const postBody = post.body;
-  body.textContent = textTruncate(postBody, 270);
+  body.textContent = textTruncate(postBody, 230);
 
   const button = document.createElement("button");
   button.classList.add("button-carousel", "font-secondary", "uppercase");
   button.textContent = "Read More";
-  button.addEventListener("click", (event) => {
-    event.preventDefault();
-    const postId = post.id;
-    const newUrl = `./post/index.html?postId=${postId}`;
-    window.location.assign(newUrl);
-  });
+  // button.addEventListener("click", (event) => {
+  //   event.preventDefault();
+  //   const postId = post.id;
+  //   const newUrl = `./post/index.html?postId=${postId}`;
+  //   window.location.assign(newUrl);
+  // });
 
   contentContainer.append(title, body, button);
   newArticleContainer.append(img, contentContainer);
