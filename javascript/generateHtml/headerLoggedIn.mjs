6b5cbefdab98045a18e0d081d3108d9e389e1377
EliNygard <eli.nygard@gmail.com>
2024-05-25 09:27:51 +0200

    adding link path to manage

--- javascript/generateHtml/headerLoggedIn.mjs
+++ javascript/generateHtml/headerLoggedIn.mjs
@@ -14,42 +14,51 @@ export async function generateHeaderLoggedInHtml() {
   linkHome.setAttribute("title", "HotView Labs Blog");
   linkHome.textContent = "HotView Labs";
   linkHome.addEventListener("click", () => {
-    const currentPath = window.location.pathname
-    if(currentPath.includes("/post") || currentPath.includes("/account")) {
-      window.location.href = "../index.html"
+    const currentPath = window.location.pathname;
+    if (currentPath.includes("/post") || currentPath.includes("/account")) {
+      window.location.href = "../index.html";
     } else {
-      window.location.href = "index.html"
+      window.location.href = "index.html";
     }
-  })
+  });
 
   const rightContainer = document.createElement("div");
   rightContainer.classList.add("content-right");
 
-  const linkManage = document.createElement("a")
-  linkManage.setAttribute("title", "Manage your posts")
+  const linkManage = document.createElement("a");
+  linkManage.setAttribute("title", "Manage your posts");
   const userName = JSON.parse(localStorage.getItem("userName"));
-  linkManage.textContent = removeUnderscore(userName)
+  linkManage.textContent = removeUnderscore(userName);
   linkManage.addEventListener("click", () => {
-    const currentPath = window.location.pathname
-    if(!currentPath.includes("/post") || currentPath.includes("/account")) {
-      window.location.href = "../post/manage.html"
-    } else {
-      window.location.href = "manage.html"
+    let targetPath = "./post/manage.html";
+
+    if (window.location.pathname.includes("/post")) {
+      targetPath = "./manage.html";
+    } else if (window.location.pathname.includes("/account")) {
+      targetPath = "../post/manage.html";
     }
-  })
+
+    window.location.href = targetPath;
+    //   const currentPath = window.location.pathname
+    //   if(!currentPath.includes("/post") || currentPath.includes("/account")) {
+    //     window.location.href = "../post/manage.html"
+    //   } else {
+    //     window.location.href = "manage.html"
+    //   }
+  });
 
   const logOutBtn = document.createElement("button");
   logOutBtn.classList.add("button", "button-small");
   logOutBtn.textContent = "Log out";
   logOutBtn.addEventListener("click", () => {
-    logOut()
-    const currentPath = window.location.pathname
-    if(currentPath.includes("/post") || currentPath.includes("/account")) {
-      window.location.href = "../index.html"
+    logOut();
+    const currentPath = window.location.pathname;
+    if (currentPath.includes("/post") || currentPath.includes("/account")) {
+      window.location.href = "../index.html";
     } else {
-      window.location.href = "index.html"
+      window.location.href = "index.html";
     }
-  })
+  });
 
   rightContainer.append(linkManage, logOutBtn);
   leftContainer.appendChild(linkHome);
